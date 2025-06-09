26feffa0fd222422cc980287da6fcb39b77a8062
Victor Gm <Victorgm1976@hotmail.com>
2025-06-09 06:00:52 +0200

    First commit. Added .gitignore

---
+++ 3d-animated-portfolio/src/components/hero/Speech.jsx
@@ -0,0 +1,9 @@
+const Speech = () => {
+  return (
+    <div className='bubbleContainer'>
+      <div className="bubble">Victor G.</div>
+      <img  src="../../../public/foto_reduce2.jpg"></img>
+    </div>
+  )
+}
+export default Speech
