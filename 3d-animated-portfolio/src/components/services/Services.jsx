cdd97672c92e4c07126566cec3edbffae4769f02
Victor Gm <bitxogm1976@gmail.com>
2025-10-21 13:18:43 +0200

    feat: New README

--- 3d-animated-portfolio/src/components/services/Services.jsx
+++ 3d-animated-portfolio/src/components/services/Services.jsx
@@ -52,28 +52,7 @@ const serviceVariants = {
 }
 
 
-// const services = [
-//   {
-//     id: 1,
-//     img: "/service1.png",
-//     title: "Web Development",
-//     counter: 35,
-//   },
 
-//   {
-//     id: 2,
-//     img: "/service2.png",
-//     title: "Product Design",
-//     counter: 13,
-//   },
-
-//   {
-//     id: 3,
-//     img: "/service3.png",
-//     title: "Branding",
-//     counter: 15,
-//   },
-// ];
 
 const services = [
   {
